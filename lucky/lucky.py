1fa374b6d6e21a63abef8f4c49af5c432a879efd
maverickkamal <musakamalmuhammad@gmail.com>
2025-07-24 17:04:18 +0100

    feat: add input frame and field to calculator window

--- lucky/lucky.py
+++ lucky/lucky.py
@@ -5,7 +5,22 @@ win = Tk()
 win.title("calculator")
 win.geometry("515x365")
 
-win.mainloop()
 win.resizable(0, 0)
+
+input_frame=  Frame(win, width=515, height=50)
+input_frame.pack(side=TOP)
+
+input_field=Entry(input_frame, font=("arial", 18, "bold"), width=45, justify=RIGHT)
+input_field.grid(row=0, column=0)
+
+
+
+
+
+
+
+
+win.mainloop()
+
 
 
